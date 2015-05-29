8d1b56fd34c392e606c0b35a18f7296037b7e6ad
kdgorospe <kdgorospe@gmail.com>
2015-05-28 16:08:11 -1000

    Single species analysis doesn't work with SAND
    For single species analysis, column name for "SANDcover" matches
    "SANDperch". Changed "SAND" to "SANDCOVER" to avoid errors.
    
    Also changed (0,20] to [0,20]

--- fish_paste/munge/02_Fish_Base_select_summary_fish_metrics.R
+++ fish_paste/munge/02_Fish_Base_select_summary_fish_metrics.R
@@ -13,9 +13,9 @@ species_table<-Aggregate_InputTable(wd, FISH_SPECIES_FIELDS)
 # SELECT SUMMARY METRICS --------------------------------------------------
 
 # calc pooled site biomass by consumer group, species, and common family
-r1<-Calc_Site_Bio(wd, "TROPHIC_MONREP");
-        trophic.cols<-levels(species_table$TROPHIC_MONREP)
-#r2a<-Calc_Site_Abund(wd, "SPECIES"); species.cols<-levels(species_table$SPECIES)
+#r1<-Calc_Site_Bio(wd, "TROPHIC_MONREP");
+#        trophic.cols<-levels(species_table$TROPHIC_MONREP)
+r2a<-Calc_Site_Abund(wd, "SPECIES"); species.cols<-levels(species_table$SPECIES)
 #r2b<-Calc_Site_Bio(wd, "SPECIES"); species.cols<-levels(species_table$SPECIES)
 #r3<-Calc_Site_Bio(wd, "COMMONFAMILYALL"); family.cols<-levels(species_table$COMMONFAMILYALL)
 r4b<-Calc_Site_Bio_By_SizeClass(wd, c(0,20,50,Inf));
@@ -25,16 +25,21 @@ r4b<-Calc_Site_Bio_By_SizeClass(wd, c(0,20,50,Inf));
 #Merge Site Data and Count Data Per Site Per Grouping Variable
 # (e.g. Species, Tropic_MonRep, Family)
 xx<-merge(survey_table, survey_est_benthos, by=UNIQUE_SURVEY)
-wsd<-merge(xx,r1,by=UNIQUE_SURVEY)
-wsd$TotFish<-rowSums(wsd[,trophic.cols])
-data.cols<-c(trophic.cols, "TotFish", SURVEY_SITE_DATA)
+if ("SAND"%in%names(r2a))
+{
+ names(xx)[grep("SAND", names(xx))]<-"SAND_COVER"
+ SURVEY_SITE_DATA[grep("SAND", SURVEY_SITE_DATA)]<-"SAND_COVER"
+}
+wsd<-merge(xx,r2a,by=UNIQUE_SURVEY)
+wsd$TotFish<-rowSums(wsd[,species.cols])
+data.cols<-c(species.cols, "TotFish", SURVEY_SITE_DATA)
 wsd<-merge(wsd, r4b, by=UNIQUE_SURVEY)
 data.cols<-c(data.cols, "0_20", "20_50", "50_plus")
 #need to make sure the cols nums are correct
 
 names(wsd)
 #names(wsd)[35:37]<-c("0_20", "20_50", "50_plus")
-names(wsd)[match(c("(0,20]", "(20,50]","(50,Inf]" ),
+names(wsd)[match(c("[0,20]", "(20,50]","(50,Inf]" ),
                  names(wsd))] <- c("0_20", "20_50", "50_plus")
 names(wsd)
 wsd$BSR<-(wsd$HARD_CORAL+wsd$CCA)/(wsd$MA + wsd$TA)
@@ -42,4 +47,3 @@ wsd$BSR<-(wsd$HARD_CORAL+wsd$CCA)/(wsd$MA + wsd$TA)
 data.cols<-c(data.cols, "BSR")
 
 cache("wsd")
-
